e36f4ebd54897bf5465d7664c51854db95e9a175
Unknown <sergei.melnik90@gmail.com>
2021-11-18 09:42:37 +0100

    Updating todo api test with 2 more tests

--- cypress/test_todo_application/API_todo_test.spec.js
+++ cypress/test_todo_application/API_todo_test.spec.js
@@ -1,10 +1,24 @@
 describe('ToDo API testing', function() {
 
-    it('return a JSON data', function() {
+    it('should return a JSON data', function() {
         cy.request('https://jsonplaceholder.typicode.com/todos')
           .its('headers')
           .its('content-type')
           .should('include', 'application/json')
+    })
+
+    it('should return task that is not completed', function() {
+        cy.request('GET', 'https://jsonplaceholder.typicode.com/todos/5')
+          .its('body')
+          .its('completed')
+          .should('be.equal', false)
+      })
+
+    it('should mark activity as complete', function() {
+      cy.request('PATCH', 'https://jsonplaceholder.typicode.com/todos/1', { completed: true })
+        .its('body')
+        .its('completed')
+        .should('be.equal', true)
     })
 
 })
